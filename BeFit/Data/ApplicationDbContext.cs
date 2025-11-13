cf0156fb0a13e71948feda2bcca588e8c1ee7ef2
xsmok <adrian.klimpel@gmail.com>
2025-11-13 18:31:43 +0100

    Refactor IdentityRole seeding to include all properties

--- BeFit/Data/ApplicationDbContext.cs
+++ BeFit/Data/ApplicationDbContext.cs
@@ -16,7 +16,13 @@ namespace BeFit.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole() { Name = "Adult", NormalizedName = "ADULT" });
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = "8895f9cd-6508-4cf7-8948-0edb4e6fd3f1",
+                Name = "Adult",
+                NormalizedName = "ADULT",
+                ConcurrencyStamp = "8895f9cd-6508-4cf7-8948-0edb4e6fd3f1"
+            });
 
 
         }
